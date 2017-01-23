7db58fd414a80b1828d1b8e304f43763ab014070
John S. Lee <jayesel@gmail.com>
2017-01-23 22:42:23 +0000

    version 0.2

--- rpsgame.py
+++ rpsgame.py
@@ -1,6 +1,10 @@
 import random
+import time
 
 player = ['rock','paper','scissors','gun','machete','nuke']
 
-for i in range( len(player)):
-    print(random.choice(player))
+for i in range(3):
+    print(i+1)
+    time.sleep(1)
+
+print(random.choice(player))
